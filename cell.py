e98c8eca4e90bb2b61d98f17585c6bfaca080ddf
Colin O'Neill <faokryn@gmail.com>
2014-02-23 22:01:59 -0500

    New Cell types DestructableWall, Start, and Finish
    DestructableWall is a wall with an HP value that can later be used to
    allow the player to break down wall.
    
    Start represents the player's starting position
    
    Finish represents the player's goal position

--- cell.py
+++ cell.py
@@ -8,6 +8,7 @@
 #   Defines the Cell class and its subclasses
 ################################################################################
 
+import random
 
 class Cell:
 #   The Cell class is the superclass of all other cell classes.  It represents a
@@ -25,8 +26,40 @@ class Cell:
     def __str__(self):
         return " "
 
+#############
+#   WALLS   #
+#############
+
 class Wall(Cell):
 #   The wall class is a subclass of the Cell class.  It represents a wall on the
-#   level.
+#   level.  By itself, it represents a wall that cannot be destroyed by the
+#   player.  These walls will be used around the outer edge of each level.
     def __str__(self):
         return u"\u2588"
+
+class DestructableWall(Wall):
+#   The DestructableWall class is a subclass of the Wall class.  It represents a
+#   wall that can be damaged by the player.  These walls will be used inside of
+#   each level.
+    def __init__(self, x, y):
+        super(DestructableWall, self).__init__(x, y)
+        self.hp = 1000
+    def __str__(self):
+        return u"\u2591"
+
+########################
+#   START AND FINISH   #
+########################
+
+class Start(Cell):
+#   The Start class represents the starting place for the player on the level.
+    def __str__(self):
+        return "S"
+
+class Finish(Cell):
+#   The Finish class represents the goal or exit for the player on the level.
+    def __str__(self):
+        return "F"
+
+
+        
