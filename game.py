2da7edb59f1c49cb53a24b0640ad3300cb13b869
Colin O'Neill <faokryn@gmail.com>
2014-03-28 23:45:21 -0400

    Added game.py
    game.py runs the game.  Currently, it uses level.py to generate a small level
    map and draws it to the window using sprites.  A character sprite can be moved
    using the arrow keys, and collision with the wall sprites stops movement.

---
+++ game.py
@@ -0,0 +1,84 @@
+import pygame, sys
+from pygame.locals import *
+from level import *
+
+CHAR_SPEED = 10
+CHAR_SIZE = 50
+CELL_SIZE = 64
+
+class Char_S(pygame.sprite.Sprite):
+	def __init__(self, levelMap):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.image = pygame.Surface([CHAR_SIZE , CHAR_SIZE])
+		self.image.fill((0,0,255))
+		self.rect = self.image.get_rect()
+		self.rect.left = CELL_SIZE + 1
+		self.rect.top = CELL_SIZE + 1
+
+	def up(self):
+		self.rect.move_ip(0, - CHAR_SPEED)
+		while pygame.sprite.spritecollide(self, levelMap, False) != []:
+			self.rect.move_ip(0, 1)
+
+	def down(self):
+		self.rect.move_ip(0, CHAR_SPEED)
+		while pygame.sprite.spritecollide(self, levelMap, False) != []:
+			self.rect.move_ip(0, -1)
+
+	def left(self):
+		self.rect.move_ip(- CHAR_SPEED, 0)
+		while pygame.sprite.spritecollide(self, levelMap, False) != []:
+			self.rect.move_ip(1, 0)
+
+	def right(self):
+		self.rect.move_ip(CHAR_SPEED, 0)
+		while pygame.sprite.spritecollide(self, levelMap, False) != []:
+			self.rect.move_ip(-1, 0)
+
+class Wall_S(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.image = pygame.Surface([CELL_SIZE, CELL_SIZE])
+		self.image.fill((0,255,0))
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = x*CELL_SIZE, y*CELL_SIZE
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+screen = pygame.display.set_mode((768, 448))
+pygame.display.set_caption("Hello World!")
+
+
+level = Level(10, 5)
+levelMap = pygame.sprite.Group()
+for cell in level.map:
+	if isinstance(level.map[cell], Wall):
+		levelMap.add(Wall_S( cell[0], cell[1] ))
+char = Char_S(levelMap)
+
+while True: # Game loop
+	for event in pygame.event.get():
+		# Check if game has been quit and quit
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pressedKeys = pygame.key.get_pressed()
+	if pressedKeys[K_UP]:
+		char.up()
+	if pressedKeys[K_DOWN]:
+		char.down()
+	if pressedKeys[K_LEFT]:
+		char.left()
+	if pressedKeys[K_RIGHT]:
+		char.right()
+
+	screen.fill(( 255, 128,   0 ))
+
+	levelMap.draw(screen)
+	pygame.draw.rect(screen, (0,0,255), char)
+
+	pygame.display.update()
+	fpsClock.tick(30)
